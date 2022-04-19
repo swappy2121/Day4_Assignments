0d27feac3638eae501e5437229877e61a466327d
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-04-19 01:24:30 -0700

    Add files via upload

---
+++ UC3_SnakeAndLadder.java
@@ -0,0 +1,56 @@
+package com.company;
+
+
+public class UC3_SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        // welcome to snake ladder program.
+
+        int currentPlayerPosition = 0;
+        int position = 0;
+
+        System.out.println("Welcome to snake ladder game");
+        System.out.println("Player will start from zero position.");
+
+        int dieRolling = (int) Math.floor(Math.random() * 10) % 7;
+        if (dieRolling == 0) {
+            dieRolling = dieRolling + 1;
+        }
+        System.out.println("Number on die is: " + dieRolling);
+        position = currentPlayerPosition + dieRolling;
+
+        int checkOptions = (int) Math.floor(Math.random() * 10) % 3;
+        switch (checkOptions) {
+
+            case 0: {
+                System.out.println("There will be no play. Player will be at same position");
+            }
+            //   System.out.println("Player is at: " + currentPlayerPosition);
+            break;
+
+            case 1: {
+                System.out.println("Player will move forward");
+                if (currentPlayerPosition < 100) {
+                    currentPlayerPosition = position;
+                }
+            }
+            System.out.println("Player is at: " + currentPlayerPosition);
+            break;
+
+            case 2: {
+                System.out.println("Player is bitten by a snake");
+                if (currentPlayerPosition > 0) {
+                    currentPlayerPosition = position - dieRolling;
+
+                } else if (currentPlayerPosition <= 0) {
+                    currentPlayerPosition = position;
+                }
+            }
+            System.out.println("Player is at: " + currentPlayerPosition);
+            break;
+
+        }
+    }
+}
+
